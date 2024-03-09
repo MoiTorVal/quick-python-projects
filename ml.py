e395600bfd8648e4acea985d4e7b88625bf140d0
moises valdez <moisesvaldez0559@gmail.com>
2024-03-09 12:34:01 -0800

    bunch of algorithms

--- ml.py
+++ ml.py
@@ -26,16 +26,17 @@
 # f100 = fib2(100)
 # print(f100)
 
-def ask_ok(prompt, retries=4, reminder='Please try again!'):
-    while True:
-        reply = input(prompt)
-        if reply in {'y', 'ye', 'yes'}:
-            return True
-        if reply in {'n', 'no', 'nop', 'nope'}:
-            return False
-        retries = retries - 1
-        if retries < 0:
-            raise ValueError('invalid user response')
-        print(reminder)
+# def ask_ok(prompt, retries=4, reminder='Please try again!'):
+#     while True:
+#         reply = input(prompt)
+#         if reply in {'y', 'ye', 'yes'}:
+#             return True
+#         if reply in {'n', 'no', 'nop', 'nope'}:
+#             return False
+#         retries = retries - 1
+#         if retries < 0:
+#             raise ValueError('invalid user response')
+#         print(reminder)
 
-ask_ok('Do you really want to quit?')
+# ask_ok('Do you really want to quit?')
+
